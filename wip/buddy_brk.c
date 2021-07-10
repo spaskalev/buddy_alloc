2f72bec879356299b3533de7c674c3429886974b
Stanislav Paskalev <spaskalev@protonmail.com>
2021-07-10 12:12:38 +0300

    wip: make buddy_brk work with stress-ng test (for initial benchmarking)

--- wip/buddy_brk.c
+++ wip/buddy_brk.c
@@ -2,70 +2,85 @@
  * Copyright 2021 Stanislav Paskalev <spaskalev@protonmail.com>
  */
 
+#include <assert.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#include "buddy_alloc.h"
+#define BUDDY_ALLOC_IMPLEMENTATION
+#include "../buddy_alloc.h"
+
 #include "buddy_global.h"
 #include "buddy_brk.h"
 
-unsigned char *global_break_start;
-unsigned char *global_break;
-struct buddy *global_buddy;
+static struct {
+    unsigned char *break_start;
+    unsigned char *break_at;
+    struct buddy *buddy;
+} global_brk = {0};
 
 void buddy_brk_init() {
-    if (global_buddy) {
+    if (global_brk.buddy) {
         return;
     }
 
     /* Get the system break */
-    global_break_start = sbrk(0);
-    if (global_break_start == NULL && errno == ENOMEM) {
+    global_brk.break_start = sbrk(0);
+    if (errno == ENOMEM) {
         return;
     }
 
     /* Bump the break to the default initial size */
-    void *break_bump = sbrk(BUDDY_BRK_DEFAULT_SIZE
-        + buddy_sizeof(BUDDY_BRK_DEFAULT_SIZE));
-    if (break_bump == NULL && errno == ENOMEM) {
+    (void) sbrk(BUDDY_BRK_DEFAULT_SIZE);
+    if (errno == ENOMEM) {
+        return;
+    }
+    /*
+     * sbrk will return the start of the new space
+     * so to get the actual break we must ask again
+     *
+     * sbrk can also increase the break beyond what's asked
+     * which is another reason to double-check it
+     */
+    global_brk.break_at = sbrk(0);
+    if (errno == ENOMEM) {
         return;
     }
 
-    global_buddy = buddy_init(
-        global_break_start + BUDDY_BRK_DEFAULT_SIZE,
-        global_break_start,
-        BUDDY_BRK_DEFAULT_SIZE
-    );
-    global_break = break_bump;
+    /* Initialize the allocator */
+    global_brk.buddy = buddy_embed(global_brk.break_start,
+        global_brk.break_at - global_brk.break_start);
 }
 
 void buddy_bump_brk() {
-    /* Get the current sizes */
-    size_t old_arena_size = buddy_arena_size(global_buddy);
-    size_t old_buddy_size = buddy_sizeof(old_arena_size);
-    size_t old_total_size = old_arena_size + old_buddy_size;
+    /* Double the break */
+    void *new_brk = sbrk(global_brk.break_at - global_brk.break_start);
+    if (errno == ENOMEM) {
+        return;
+    }
+    global_brk.break_at = sbrk(0);
+    if (errno == ENOMEM) {
+        return;
+    }
 
-    /* Get the new sizes */
-    size_t new_arena_size = old_arena_size * 2;
-    size_t new_buddy_size = buddy_sizeof(new_arena_size);
-    size_t new_total_size = new_arena_size + new_buddy_size;
+    global_brk.break_at = new_brk;
+    global_brk.buddy = buddy_resize(global_brk.buddy,
+        global_brk.break_at - global_brk.break_start);
+}
 
-    /* Raise the brk */
-    ssize_t diff = new_total_size - old_total_size;
-    global_break = sbrk(diff);
+void buddy_lower_brk(void) {
 
-    /* Resize and relocate the allocator */
-    buddy_resize(global_buddy, new_arena_size);
-    memmove(global_break_start+new_arena_size,
-        global_break_start+old_arena_size, new_buddy_size);
-    global_buddy = (struct buddy *) (global_break_start+new_arena_size);
 }
 
 void *malloc(size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
     buddy_brk_init();
     void *result = NULL;
-    while (!(result = buddy_malloc(global_buddy, size))) {
+    while (!(result = buddy_malloc(global_brk.buddy, size))) {
         buddy_bump_brk();
     }
     return result;
@@ -73,60 +88,42 @@ void *malloc(size_t size) {
 
 void free(void *ptr) {
     buddy_brk_init();
-    buddy_free(global_buddy, ptr);
-    if (buddy_can_shrink(global_buddy)) {
-        /* Get the current sizes */
-        size_t old_arena_size = buddy_arena_size(global_buddy);
-        if (old_arena_size == BUDDY_BRK_DEFAULT_SIZE) {
-            return;
-        }
-        size_t old_buddy_size = buddy_sizeof(old_arena_size);
-        size_t old_total_size = old_arena_size + old_buddy_size;
-
-        /* Get the new sizes */
-        size_t new_arena_size = old_arena_size / 2;
-        size_t new_buddy_size = buddy_sizeof(new_arena_size);
-        size_t new_total_size = new_arena_size + new_buddy_size;
-
-        /* Attempt to resize the allocator */
-        struct buddy *resized = buddy_resize(global_buddy, new_arena_size);
-        if (! resized) {
-            return; // failed to resize
-        }
-
-        /* Move it back */
-        memmove(global_break_start+new_arena_size,
-            global_break_start+old_arena_size, new_buddy_size);
-        global_buddy = (struct buddy *) (global_break_start+new_arena_size);
-
-        /* Lower the brk */
-        ssize_t diff = old_total_size - new_total_size;
-        global_break = sbrk(diff * -1);
-    }
+    buddy_free(global_brk.buddy, ptr);
 }
 
 void *calloc(size_t nmemb, size_t size) {
+    if ((nmemb == 0) || (size == 0)) {
+        return NULL;
+    }
     buddy_brk_init();
     void *result = NULL;
-    while (!(result = buddy_calloc(global_buddy, nmemb, size))) {
+    while (!(result = buddy_calloc(global_brk.buddy, nmemb, size))) {
+        assert(buddy_arena_size(global_brk.buddy));
         buddy_bump_brk();
     }
+    assert(buddy_arena_size(global_brk.buddy));
     return result;
 }
 
 void *realloc(void *ptr, size_t size) {
+    if (size == 0) {
+        return NULL;
+    }
     buddy_brk_init();
     void *result = NULL;
-    while (!(result = buddy_realloc(global_buddy, ptr, size))) {
+    while (!(result = buddy_realloc(global_brk.buddy, ptr, size))) {
         buddy_bump_brk();
     }
     return result;
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size) {
+    if ((nmemb == 0) || (size == 0)) {
+        return NULL;
+    }
     buddy_brk_init();
     void *result = NULL;
-    while (!(result = buddy_reallocarray(global_buddy, ptr, nmemb, size))) {
+    while (!(result = buddy_reallocarray(global_brk.buddy, ptr, nmemb, size))) {
         buddy_bump_brk();
     }
     return result;
---
+++ wip/gdbinit.cfg
@@ -0,0 +1,1 @@
+set style enabled off
